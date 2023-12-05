894c291a5bf54c549104789b2279a5b05466aca0
Chupaeng <villaralvorovic2@gmail.com>
2023-12-05 08:17:22 +0800

    Help I a struggling

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 import fetch from 'node-fetch';
 
 const fetchData = async (url) => {
-    const response = await fetch(url);
-    return response.json();
+    const response = await fetch('https://robohash.org/avb');
+    return response.blob();
 }
+
+fetchData().then((e) =>{
+    console.log(typeof e);
+    const img = document.createElement('img');
+    const imgURL = URL.createObjectURL(e);
+
+    console.log(imgURL)
+
+    img.setAttribute('src', 'imgURL')
+    document.querySelector('header').append(img);
+})
+
