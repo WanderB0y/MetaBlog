c10c08b26ae8500d98535b280ee0e63b156143a1
Chupaeng <villaralvorovic2@gmail.com>
2023-12-08 20:22:36 +0800

    Update fetch data and footer styling

--- app/js/index.js
+++ app/js/index.js
@@ -1,17 +1,41 @@
+const articleTitle = document.querySelector('#article--title');
+const articleAuthor = document.querySelector('#article--author');
+const articleDate = document.querySelector('#article--date');
+const articleContent = document.querySelector('#article--content');
+const articleImage = document.querySelector('#article--image');
+const loadingAnimation = document.querySelector('#loading--animation');
+const article = document.querySelector('main').firstChild.nextSibling.nextElementSibling;
 
-const arr = [];
+const fetchData = async() => {
+    try{    
+        const response = await fetch('https://newsapi.org/v2/everything?q=tesla&from=2023-11-08&sortBy=publishedAt&apiKey=07acc4f2f3284655af7e6afc4b4b2435');
 
-const fetchData = () => {
-    return new Promise ((resolve, reject) => {
-        setTimeout(() => {
-            arr.push(5),
-            document.querySelector('#article--image').setAttribute('src','https://images.wsj.net/im-896114/social')
-         }, 5000);
-})}
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+        return response.json();
+    } catch(error){
+        console.error('Unable to fetch data:', error);
+    }
+}
+
+function pickRandom(articles) {
+    return articles[Math.floor(Math.random() * articles.length)];
+}
 
 
-fetchData().then((e) => {
-    console.log(e)
+const displayContent = (articles) => {
+    loadingAnimation.style.display = 'none'
+    article.style.display = 'flex'
+
+    articleImage.setAttribute('src',articles.urlToImage)
+    articleTitle.textContent = articles.title;
+    articleAuthor.textContent = articles.source.name;
+    articleDate.textContent = articles.publishedAt;
+    articleContent.textContent = articles.content;
+}
+
+fetchData().then((result) => {
+    displayContent(pickRandom(result.articles))
 })
-
 
