f5c347f5b79daf99cc1833f534a67b159cae93d2
Chupaeng <villaralvorovic2@gmail.com>
2024-01-01 21:15:07 +0800

    Add other sections

--- server.js
+++ server.js
@@ -8,13 +8,7 @@ app.use(express.static('public'));
 
 
 app.get('/', async (req, res) => {
-    try {
-        // const response = await axios.get('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=07acc4f2f3284655af7e6afc4b4b2435')
-        res.render('index.ejs', { articles: response.data.articles })
-    } catch (error) {
-        res.render('index.ejs', { message: `Failed fetching the data: ${error}` })
-        console.log(error)
-    }
+    res.render('index.ejs')
 })
 
 app.get('/blog', (req, res) => {
